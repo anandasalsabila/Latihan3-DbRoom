18f63a54a17e62fb93d379e7b8690eaea623a46f
Adhitya <adhityarpr00@gmail.com>
2020-05-02 00:02:28 +0700

    DbRoom-Caca

---
+++ app/src/main/java/com/caca/latihan3_akb/AppDatabase.java
@@ -0,0 +1,12 @@
+package com.caca.latihan3_akb;
+
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+
+@Database(entities = {AktivisEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract AktivisDao aktivisDao();
+}
